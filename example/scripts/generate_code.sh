2acdd99ff938c647d7d5843d9afd6f431383d5d4
kwado-tech <kwado.tech@gmail.com>
2023-12-05 18:39:18 +0100

    [FEATURE] Refactored scripts to simplify `l10_mapper_generator` call.

---
+++ example/scripts/generate_code.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# generate localization-files
+(cd ../ && flutter gen-l10n)
+
+# annotate `app_localizations` to generate l10_mapper-part file
+(cd ../ && dart pub run l10_mapper_generator --source=lib/localization/gen-l10n/app_localizations.dart)
+
+# generate code
+(cd ../ && flutter pub run build_runner build --delete-conflicting-outputs)
