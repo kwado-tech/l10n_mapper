5f6c3944cebed0c4e220331f8ef1263ecc1582e8
Christian Chikwado Onwe <47870274+kwado-tech@users.noreply.github.com>
2023-12-05 17:00:18 +0100

    [FEATURE] Migrated scripts to `l10_mapper_generator` package (#1)
    * [FEATURE] Migrate scripts to `l10_mapper_generator` package. This is to enable implicit calling of scripts when required.
    
    * [FEATURE] Added parsable file-path argument to `AnnotateLocalization` script,
    Slight refactor.
    
    * [FEATURE]
    - Fixed error message,
    - Fixed spacing issues after annotation.

---
+++ l10_mapper_generator/lib/scripts/annotate_localization.dart
@@ -0,0 +1,89 @@
+import 'dart:io';
+
+// required for isolated testing
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print("Error: Not enough arguments provided.");
+    print("Usage: dart annotate_localization.dart <file_path>");
+    exit(1);
+  }
+
+  return AnnotateLocalization()(arguments[0]);
+}
+
+class AnnotateLocalization {
+  void call(String filePath) {
+    // Mapper generator-config options
+    String searchParameter = 'abstract class AppLocalizations {\n';
+
+    String requiredImports = '''
+import 'package:l10_mapper_annotation/l10_mapper_annotation.dart';
+part 'app_localizations.g.dart';
+
+@L10MapperAnnotation()
+abstract class AppLocalizations {
+''';
+
+    // Write imports and annotations to app_localization.dart file
+    print('\nAdding required imports to generated app_localizations');
+    replaceString(
+        path: filePath, pattern: searchParameter, replacement: requiredImports);
+  }
+
+  void replaceContent({
+    required String path,
+    required String pattern,
+    required String replacement,
+    required String fileContent,
+  }) {
+    final replacedContent = fileContent.replaceAll(
+        RegExp(pattern, caseSensitive: false), replacement);
+    final tempFile = File('$path.tmp');
+    tempFile.writeAsStringSync(replacedContent);
+
+    // Check if the replacement was successful
+    if (tempFile.existsSync()) {
+      // Overwrite the original file with the new file
+      tempFile.renameSync(path);
+
+      print("Replacement completed successfully.");
+    } else {
+      print("Error: Replacement failed.");
+      exit(1);
+    }
+  }
+
+  void replaceString({
+    required String path,
+    required String pattern,
+    required String replacement,
+  }) {
+    // Check if the input file exists
+    final inputFile = File(path);
+    if (!inputFile.existsSync()) {
+      print("Error: Input file does not exist.");
+      exit(1);
+    }
+
+    // Backup the original file
+    final backupFile = File('$path.bak');
+    inputFile.copySync(backupFile.path);
+
+    // Perform the search and replace and write the result to a new file
+    final fileContent = inputFile.readAsStringSync();
+
+    // verify if replacement operation was previously successful
+    final alreadyReplaced = fileContent.contains(replacement);
+    if (alreadyReplaced) {
+      print("Error: AnnotateLocalization failed as specified replacement already exists!");
+      exit(1);
+    }
+
+    replaceContent(
+      path: path,
+      pattern: pattern,
+      replacement: replacement,
+      fileContent: fileContent,
+    );
+  }
+}
