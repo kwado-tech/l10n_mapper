aec217fc43f47be3cc1e07009fedd09670d0214a
Christian Chikwado Onwe <47870274+kwado-tech@users.noreply.github.com>
2023-12-06 00:31:07 +0100

    [FEATURE] Implemented minimal logging framework (#3)
    * [FEATURE] implemented minimal logging-framework.
    
    * Slight fix.

--- l10_mapper_generator/bin/l10_mapper_generator.dart
+++ l10_mapper_generator/bin/l10_mapper_generator.dart
@@ -2,38 +2,34 @@ import 'dart:io';
 
 import 'package:args/args.dart';
 
+import '../core/logger.dart';
 import '../scripts/annotate_localization.dart';
 
 void main(List<String> arguments) {
-  // TODO: write expressive help-messages
   final parser = ArgParser()
     ..addFlag('help',
-        abbr: 'h', negatable: false, help: 'Print this usage information')
+        abbr: 'h',
+        negatable: false,
+        help: 'Usage: dart pub run l10_mapper_generator [options]')
     ..addOption('source',
         abbr: 's',
-        help: 'Provide path of generated `app_localization.dart` file');
+        help: 'Provide the path of the generated `app_localization.dart` file');
 
   try {
     final results = parser.parse(arguments);
 
     if (results['help'] as bool) {
-      print('Usage: dart pub run l10_mapper_generator [options]');
-      print(parser.usage);
-
-      exit(0);
+      logger(parser.usage, () => exit(0), type: LogType.usage);
     }
 
     final source = results['source'] as String?;
 
     if (source == null || source.isEmpty) {
-      print('Error: ${parser.usage}');
-
-      exit(1);
+      logger(parser.usage, () => exit(1), type: LogType.error);
     }
 
     return AnnotateLocalization()(results['source']);
   } catch (e) {
-    print('Error: $e');
-    exit(1);
+    logger(e.toString(), () => exit(1), type: LogType.error);
   }
 }
