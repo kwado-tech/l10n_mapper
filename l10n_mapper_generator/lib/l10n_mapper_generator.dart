4b629f0854af6e0a2d4b8561cfe36b025347ddff
reinhardplaim <reinhard.plaim@engel.at>
2025-01-24 13:21:41 +0100

    support multiple translation files #34
    support multiple translation files #34

--- l10n_mapper_generator/lib/l10n_mapper_generator.dart
+++ l10n_mapper_generator/lib/l10n_mapper_generator.dart
@@ -17,68 +17,103 @@ class L10nMapperGenerator extends Generator {
   final bool l10n;
   final bool locale;
   final bool parseL10n;
+  List<String> classNames;
 
   //? optional and default: null. should be parsed if translation should not return
   //? nullable values when key is not found but will return specified error message instead
   final String? message;
 
-  L10nMapperGenerator({required this.l10n, required this.locale, required this.parseL10n, required this.message});
+  L10nMapperGenerator({required this.l10n, required this.locale, required this.parseL10n, required this.message, this.classNames = const []});
 
   @override
   FutureOr<String?> generate(LibraryReader library, BuildStep buildStep) {
+    if (classNames.isEmpty) {
+      classNames.add('AppLocalizations');
+    }
+
     final buffer = StringBuffer();
 
     for (var classElement in library.classes.where((c) => c.isAbstract)) {
-      if (classElement.displayName == 'AppLocalizations') {
+      if (classNames.contains(classElement.displayName)) {
         final className = classElement.displayName;
         final localizationPath = classElement.source.uri;
         final mapperName = '${classElement.displayName}Mapper';
-        final extensionName = '${classElement.displayName}Extension';
+        final appLocalizationsExtensionName = '${classElement.displayName}Extension';
+        final buildContextExtensionName = 'BuildContextExtension';
 
         final nullable = message == null;
-        final shouldGenerateExtension = l10n || locale || parseL10n;
+        final shouldGenerateExtensions = l10n || locale || parseL10n;
 
         // import
         buffer.writeln("import '$localizationPath';");
         buffer.writeln("import 'package:flutter/widgets.dart';");
 
-        // generate extension
-        if (shouldGenerateExtension) {
-          buffer.writeln('extension $extensionName on BuildContext {');
-          buffer.writeln('  $className get _localizations => $className.of(this)!;');
+        // generate extensions
+        if (shouldGenerateExtensions) {
+          final bufferBuildContextExtension = StringBuffer();
+          final bufferAppLocalizationsExtension = StringBuffer();
+          final bufferL10nHelper = StringBuffer();
+
+          bufferBuildContextExtension.writeln('extension $buildContextExtensionName on BuildContext {');
+          bufferAppLocalizationsExtension.writeln('extension $appLocalizationsExtensionName on ${classElement.displayName} {');
+
+          bufferBuildContextExtension.writeln('  $className get _localizations => $className.of(this)!;');
 
           if (l10n) {
-            buffer.writeln('$className get l10n => _localizations;');
+            bufferBuildContextExtension.writeln('$className get l10n => _localizations;');
           }
 
           if (locale) {
-            buffer.writeln('Locale get locale => Localizations.localeOf(this);');
+            bufferBuildContextExtension.writeln('Locale get locale => Localizations.localeOf(this);');
           }
 
           if (parseL10n) {
-            buffer.writeln(
+            bufferBuildContextExtension.writeln(
                 "${nullable ? 'String?' : 'String'} parseL10n(String translationKey, {List<Object>? arguments}) {");
-            buffer.writeln('const mapper = $mapperName();');
-            buffer.writeln('final object = mapper.toLocalizationMap(this)[translationKey];');
+            bufferAppLocalizationsExtension.writeln(
+                "${nullable ? 'String?' : 'String'} parseL10n(String translationKey, {List<Object>? arguments}) {");
+
+            bufferBuildContextExtension.writeln('final localizations = $className.of(this)!;');
+            bufferBuildContextExtension.writeln('return L10nHelper.parseL10n(localizations, translationKey, arguments: arguments);');
+            bufferAppLocalizationsExtension.writeln('return L10nHelper.parseL10n(this, translationKey, arguments: arguments);');
+
+            bufferBuildContextExtension.writeln('}');
+            bufferAppLocalizationsExtension.writeln('}');
+
+            bufferL10nHelper.writeln('class L10nHelper {');
+            bufferL10nHelper.writeln('static String parseL10n(AppLocalizations localizations, String translationKey, {List<Object>? arguments}) {');
+
+            bufferL10nHelper.writeln('const mapper = $mapperName();');
+            bufferL10nHelper.writeln('final object = mapper.toLocalizationMap(localizations)[translationKey];');
 
             // account for null-case
             if (!nullable) {
-              buffer.writeln("if (object == null) return '$message';");
+              bufferL10nHelper.writeln("if (object == null) return '$message';");
 
-              buffer.writeln('if (object is String) return object;');
+              bufferL10nHelper.writeln('if (object is String) return object;');
             } else {
-              buffer.writeln('if (object is String || object == null) return object;');
+              bufferL10nHelper.writeln('if (object is String || object == null) return object;');
             }
 
-            buffer.writeln("assert(arguments != null, 'Arguments should not be null!');");
-            buffer.writeln("assert(arguments!.isNotEmpty, 'Arguments should not be empty!');");
+            bufferL10nHelper.writeln("assert(arguments != null, 'Arguments should not be null!');");
+            bufferL10nHelper.writeln("assert(arguments!.isNotEmpty, 'Arguments should not be empty!');");
 
-            buffer.writeln('return Function.apply(object, arguments);');
-            buffer.writeln('}');
+            bufferL10nHelper.writeln('return Function.apply(object, arguments);');
+            bufferL10nHelper.writeln('}');
+
+            bufferL10nHelper.writeln('}');
           }
 
-          buffer.writeln('}');
+          bufferBuildContextExtension.writeln('}');
+          bufferAppLocalizationsExtension.writeln('}');
+
+          buffer
+            ..write(bufferBuildContextExtension.toString())
+            ..write(bufferAppLocalizationsExtension.toString())
+            ..write(bufferL10nHelper.toString());
         }
+
+
         // end of extension
 
         // generate class
@@ -88,8 +123,7 @@ class L10nMapperGenerator extends Generator {
         buffer.writeln('const $mapperName();');
 
         // toLocalizationMap
-        buffer.writeln('Map<String, dynamic> toLocalizationMap(BuildContext context) {');
-        buffer.writeln('    final localizations = $className.of(context)!;');
+        buffer.writeln('Map<String, dynamic> toLocalizationMap(${classElement.displayName} localizations) {');
 
         buffer.writeln('return {');
         // all getters
